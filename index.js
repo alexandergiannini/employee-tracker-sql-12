88d5d1e2694ea090e387905f76ec71ed875f48e9
Alexander Giannini <alexgiannini@alexanders-mbp.home>
2021-03-06 13:48:31 -0800

    added functionality to update employee roles

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const Database = require('./server.js')
-//const cTable = require('console.table');
-// const connection = require('./server.js')
-// const showAllDepartments = require('./server.js')
-// const showAllRoles = require('./server.js')
-// const showAllEmployees = require('./server.js')
-
-// let showAllDepartments = () => {
-//     return new Promise ((resolve, reject) => {
-//      connection.query(
-//          'SELECT * FROM department;',
-//          function (err, results, fields) {
-//            if (err) {
-//              reject(err)
-//            } else {
-//           // let departmentTable = cTable([
-//            //    {
-               
-//              //  }
-//           // ])
-//            resolve(results)
-//             // return console.log(results)
-//             //return results
-//            }
-//          }
-//     )
-//      })
-//    }
+const Database = require('./server.js');
 
 
 const promptAllOptions = () => {
@@ -38,26 +10,11 @@ const promptAllOptions = () => {
         message: 'What would you like to do?',
         choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
         },
-        // {
-        // name: 'viewDepartments',
-        // message: showAllDepartments, ///need to input function here
-        // when: (data) => data.allOptions === 'View All Departments'
-        // },
-        // {
-        // name: 'viewRoles',
-        // message: showAllRoles, ///need to input function here
-        // when: (data) => data.allOptions === 'View All Roles'
-        // },
-        // {
-        // name: 'viewEmployees',
-        // message: showAllEmployees, ///need to input function here
-        // when: (data) => data.allOptions === 'View All Employees'
-        // },
         {
          type: 'input',
          name: 'departmentName',
          message: 'What is the name of the department you would like to add?', 
-         when: (data) => data.allOptions === 'Add a Department' ///need to add new dpt to database
+         when: (data) => data.allOptions === 'Add a Department' 
          },
          {
          type: 'input',
@@ -75,7 +32,7 @@ const promptAllOptions = () => {
          type: 'input',
          name: 'roleDepartment',
          message: 'What is the department for the role?',
-         when: (data) => data.allOptions === 'Add a Role' ///need to add role info to the database
+         when: (data) => data.allOptions === 'Add a Role' 
          },
          {
          type: 'input',
@@ -105,21 +62,20 @@ const promptAllOptions = () => {
          type: 'input',
          name: 'employeeManager',
          message: 'Who is the manager of the employee?',
-         when: (data) => data.allOptions === 'Add an Employee' ///need to add employee info into the database
+         when: (data) => data.allOptions === 'Add an Employee' 
          },
-        // {
-        // type: 'list',
-        // name: 'whichEmployee',
-        // message: 'Which Employee would you like to update?',
-        // choices: ['test 1', 'test 2'], ///need to add list of employees here
-        // when: (data) => data.allOptions === 'Update an Employee Role'
-        // },
-        // {
-        // type: 'input',
-        // name: 'employeeRoleUpdate',
-        // message: 'Please input which role you would like to update for this employee',
-        // when: (data) => data.allOptions === 'Update an Employee Role' //need to update role of the employee after this is answered
-        // }
+         {
+         type: 'input',
+         name: 'whichEmployee',
+         message: 'Which Employee would you like to update?',
+         when: (data) => data.allOptions === 'Update an Employee Role'
+         },
+         {
+         type: 'input',
+         name: 'employeeRoleUpdate',
+         message: 'Please input which role you would like to update for this employee:',
+         when: (data) => data.allOptions === 'Update an Employee Role' 
+         }
     ])
     .then(answers => {
         switch (answers.allOptions) {
@@ -128,7 +84,6 @@ const promptAllOptions = () => {
             console.table(results)
             promptAllOptions()
             })
-            //promptAllOptions()
             break;
 
             case "View All Employees":
@@ -136,8 +91,6 @@ const promptAllOptions = () => {
             console.table(results)
             promptAllOptions()
             })
-           // promptAllOptions()
-            //loopAllOptions();
             break;
 
             case "View All Roles":
@@ -145,86 +98,37 @@ const promptAllOptions = () => {
             console.table(results)
             promptAllOptions()
             })
-
-           // promptAllOptions()
-            //loopAllOptions();//
             break;
 
             case "Add a Department":
             Database.addDepartment(answers.departmentName).then((results) => {
-            console.log("this works.")
+            console.log("Adding a department has succeeded.")
             promptAllOptions()
             })
-               // promptAllOptions()
-           // loopAllOptions();
             break;
 
             case "Add a Role":
             Database.addRole(answers.roleName, answers.roleSalary, answers.roleDepartment).then((results) => {
-            console.log('Adding a role succeeded.')
+            console.log('Adding a role has succeeded.')
             promptAllOptions()
             })
-           // promptAllOptions()
-            //loopAllOptions();
             break;
 
             case "Add an Employee":
             Database.addEmployee(answers.employeeFirst, answers.employeeLast, answers.employeeRole, answers.employeeDepartment, answers.employeeManager).then((results) => {
-            console.log('Adding an employee succeeded.')
+            console.log('Adding an employee has succeeded.')
             promptAllOptions()
             })
-            //promptAllOptions()
-           // loopAllOptions();
             break;
 
             case "Update an Employee Role":
+            Database.updateEmployee(answers.whichEmployee, answers.employeeRoleUpdate).then((results) => {
+            console.log('Employee role has been updated.')
             promptAllOptions()
-            //loopAllOptions();
+            })
             break;
-
         }
     })
 }
 
-// promptAllOptions().then(sampleAnswer => {
-//     loopAllOptions()
-
-//
-
-const loopAllOptions = () => {
-//     promptAllOptions().then(answers => {
-//         switch (answers.allOptions) {
-//             case "View All Departments":
-//             showAllDepartments()
-//             loopAllOptions();
-//             break;
-
-//             case "View All Employees":
-//             loopAllOptions();
-//             break;
-
-//             case "View All Roles":
-//             loopAllOptions();
-//             break;
-
-//             case "Add a Department":
-//             loopAllOptions();
-//             break;
-
-//             case "Add a Role":
-//             loopAllOptions();
-//             break;
-
-//             case "Add an Employee":
-//             loopAllOptions();
-//             break;
-
-//             case "Update an Employee Role":
-//             loopAllOptions();
-//             break;
-
-//         }
-//     })
- }
 promptAllOptions()
-//
--- server.js
+++ server.js
@@ -17,10 +17,6 @@ class Database {
 
             this.connection.connect(err => {
                 if (err) throw err;
-               // console.log('connected as id ' + connection.threadId);
-                // showAllDepartments();
-                // showAllRoles();
-                // showAllEmployees();
               });
     }
 
@@ -65,17 +61,10 @@ class Database {
             if (err) {
               reject(err)
             } else {
-           // let departmentTable = cTable([
-            //    {
-                
-              //  }
-           // ])
             resolve(results)
-             // return console.log(results)
-             //return results
             }
           }
-     )
+        )
       })
     }
 
@@ -87,18 +76,11 @@ class Database {
                   if (err) {
                     reject(err)
                   } else {
-                 // let departmentTable = cTable([
-                  //    {
-                      
-                    //  }
-                 // ])
                   resolve(results)
-                   // return console.log(results)
-                   //return results
                   }
                 }
            )
-            })
+        })
     }
 
     addRole = (title, salary, departmentName) => {
@@ -130,102 +112,23 @@ class Database {
             )
         })
     }
-    /// EXAMPLE -> 'INSERT INTO employee(first_name, last_name, role_id, jobTitle, departmentName, salary, manager) VALUES (?, ?, ?, ?, ?, ?, ?), ['argument 1', 'argument 2', 'arg3'],
-    /// 
+
+    updateEmployee = (firstName, jobTitle) => {
+        return new Promise ((resolve, reject) => {
+            this.connection.query(
+                'UPDATE employee SET jobTitle = ? WHERE first_name = ?', [jobTitle, firstName],
+                function (err, results, fields) {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(results)
+                    }
+                }
+            )
+        })
+    }
 }
 
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'root',
-//   password: 'Tenniskid1',
-//   database: 'work_db'
-// });
-
-
-
-//   showAllEmployees = () => {
-//       connection.query(
-//           'SELECT * FROM employee;',
-//           function (err, results, fields) {
-//               if (err) {
-//                   throw err
-//               } else {
-//                  // console.log(results)
-//                   //return console.log(results)
-//                   return results
-//               }
-//           }
-//       )
-//   }
-
-//   showAllRoles = () => {
-//     connection.query(
-//         'SELECT * FROM roles;',
-//         function (err, results, fields) {
-//             if (err) {
-//                 throw err
-//             } else {
-//                // console.log(results)
-//                 return results
-//             }
-//         }
-//     )
-// }
-
-
-
-//  let showAllDepartments = () => {
-//    return new Promise ((resolve, reject) => {
-//     connection.query(
-//         'SELECT * FROM department;',
-//         function (err, results, fields) {
-//           if (err) {
-//             reject(err)
-//           } else {
-//          // let departmentTable = cTable([
-//           //    {
-              
-//             //  }
-//          // ])
-//           resolve(results)
-//            // return console.log(results)
-//            //return results
-//           }
-//         }
-//    )
-//     })
-//   }
-    // connection.query(
-    //   'SELECT * FROM department;',
-    //   function (err, results, fields) {
-    //     if (err) {
-    //       throw err
-    //     } else {
-    //    // let departmentTable = cTable([
-    //     //    {
-            
-    //       //  }
-    //    // ])
-    //     console.log(results)
-    //      // return console.log(results)
-    //      //return results
-    //     }
-    //   }
-  
-    // )
-
-
-
-
-//connection.end(); using this temporarily
-    
-
-
  module.exports = new Database()
-//   module.exports = connection
-//   module.exports = showAllDepartments
-//   module.exports = showAllRoles 
-//   module.exports = showAllEmployees
-  
+
   
